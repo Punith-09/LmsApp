0dbbb03cf815034b6fa3d74944e4217c3c204031
Punith Gopishetti <punith.gopishetti@gmail.com>
2025-11-16 02:15:56 +0530

    Disable ESLint blocking build

--- lms/next.config.ts
+++ lms/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+  reactStrictMode: true, // keep this if you want
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
